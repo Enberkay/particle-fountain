5f82aa9ccd966bf4057bae7b8ba460603f950128
Celine <enberkaych@gmail.com>
2025-11-16 23:53:08 +0700

    Step 2: Add Particle struct definition

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
 use macroquad::prelude::*;
+use macroquad::rand::gen_range;
+
+// Particle structure to represent each particle in our fountain
+#[derive(Debug)]
+struct Particle {
+    // Position (x, y)
+    pos: Vec2,
+    // Velocity (x, y)
+    vel: Vec2,
+    // Color of the particle
+    color: Color,
+    // How long the particle will live (in seconds)
+    lifetime: f32,
+    // Maximum lifetime for this particle
+    max_lifetime: f32,
+    // Size of the particle
+    size: f32,
+}
 
 #[macroquad::main("Particle Fountain")]
 async fn main() {
